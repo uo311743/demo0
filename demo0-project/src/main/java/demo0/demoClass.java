6a38fbe2c899aa07e3a457a5a33b7e64638ec100
uo311743 <uo311743@uniovi.es>
2025-02-11 15:51:57 +0100

    Update demoClass.java
    Hello World from remote

--- demo0-project/src/main/java/demo0/demoClass.java
+++ demo0-project/src/main/java/demo0/demoClass.java
@@ -3,8 +3,6 @@ package demo0;
 public class demoClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Hello World from remote");
 	}
-
 }
